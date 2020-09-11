dbbe1a629d1aa5cc1e31b28617b6372b8f4e252d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-10 21:44:32 -0300

    Add search route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,13 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function search(Request $request)
+    {
+        // $messages = Message::all();
+        $githubClient = new \App\Api\GithubApiClient();
+        $results = $githubClient->get("search/repositories?q={$request->q}&per_page=10");
+        
+        return view('home', compact('results'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', '\App\Http\Controllers\HomeController@index');
+Route::post('/search', '\App\Http\Controllers\HomeController@search');
